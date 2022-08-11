29a7d94b5726a46d7bab071e61bba06984991453
rohithpala <rohithpala02@gmail.com>
2022-08-11 17:05:14 +0530

    Inbuilt Sorrting Algorithm

--- Sorting-Techniques/qsort.c
+++ Sorting-Techniques/qsort.c
@@ -2,7 +2,7 @@
 #include<stdlib.h> // Contains the inbuilt library sorting technique qsort
 
 // nlogn time complexity
-void* cmp(int* a, int* b){ // We encounter a warning
+void* cmp(int* a, int* b) { // We encounter a warning
      // Ascending Order
      // if(*a - *b <= 0) return -1;
      // else if(*a - *b == 0) return 0;
@@ -14,7 +14,7 @@ void* cmp(int* a, int* b){ // We encounter a warning
      return -1;
 }
 
-void main(){
+void main() {
      int a[] = {10, 15, 5, 1, 12, 2, 7, 6};
      int i, len = sizeof(a)/sizeof(a[0]);
      qsort(a, len, sizeof(a[0]), cmp); // cmp is comparator
